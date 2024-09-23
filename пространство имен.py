a3038a80296547a276974c563c50f482a8c2ee33
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-09-23 18:15:29 +0500

    Add files via upload

---
+++ пространство имен.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print('Я в области видимости функции test_function')
+    inner_function()
+
+
+print(test_function())
+# print(inner_funktion())
